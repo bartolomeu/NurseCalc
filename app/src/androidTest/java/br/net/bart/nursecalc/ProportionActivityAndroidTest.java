86b86cbdaa8678656a4608e420dd022e86a7ec57
Bartolomeu Spegiorin Gusella <ccbarto@gmail.com>
2017-11-17 16:38:15 -0200

    Ajustes nos tests

--- app/src/androidTest/java/br/net/bart/nursecalc/ProportionActivityAndroidTest.java
+++ app/src/androidTest/java/br/net/bart/nursecalc/ProportionActivityAndroidTest.java
@@ -1,12 +1,16 @@
 package br.net.bart.nursecalc;
 
+import android.support.test.runner.AndroidJUnit4;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by bartolomeu.gusella on 17/11/2017.
  */
+@RunWith(AndroidJUnit4.class)
 public class ProportionActivityAndroidTest {
     @Test
     public void validateAllETProps() throws Exception {
--- app/src/test/java/br/net/bart/nursecalc/ProportionActivityTest.java
+++ app/src/test/java/br/net/bart/nursecalc/ProportionActivityTest.java
@@ -1,5 +1,9 @@
 package br.net.bart.nursecalc;
 
+import android.util.Log;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,28 +12,47 @@ import static org.junit.Assert.*;
  * Created by bartolomeu.gusella on 14/11/2017.
  */
 public class ProportionActivityTest {
+
+    protected ProportionActivity proportionActivity;
+
+    @Before
+    public void before(){
+        proportionActivity = new ProportionActivity();
+    }
+
+    //checkProportion()
     @Test
-    public void onCreate() throws Exception {
+    public void checkProportion_integers() throws Exception {
+        assertEquals(true, proportionActivity.checkProportion(1, 2, 3, 6));
     }
 
     @Test
-    public void validateAllETProps() throws Exception {
+    public void checkProportion_integersWrong() throws Exception {
+        assertEquals(false, proportionActivity.checkProportion(1, 2, 3, 5));
     }
 
     @Test
-    public void calculate() throws Exception {
+    public void checkProportion_decimals() throws Exception {
+        assertEquals(true, proportionActivity.checkProportion(5f, 7f, 28.0f, 39.2f));
     }
 
     @Test
-    public void checkProportion() throws Exception {
+    public void checkProportion_fristZero() throws Exception {
+        assertEquals("Validação com 0 no 1° campo", false, proportionActivity.checkProportion(0, 2, 3, 6));
     }
 
     @Test
-    public void setEditTextProportion() throws Exception {
+    public void checkProportion_secondZero() throws Exception {
+        assertEquals("Validação com 0 no 2° campo", false, proportionActivity.checkProportion(1, 0, 3, 6));
     }
 
     @Test
-    public void clearErrorFields() throws Exception {
+    public void checkProportion_thirdZero() throws Exception {
+        assertEquals("Validação com 0 no 3° campo", false, proportionActivity.checkProportion(1, 2, 0, 6));
     }
 
+    @Test
+    public void checkProportion_fourthZero() throws Exception {
+        assertEquals("Validação com 0 no 4° campo", false, proportionActivity.checkProportion(1,2,3,0));
+    }
 }
